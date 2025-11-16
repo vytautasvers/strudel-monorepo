edb6dcec8e24cc1cec5a3f35c774e8c22d47c13a
Vytautas Verseckas <vytver@boozt.com>
2025-11-16 14:22:34 +0200

    Initial monorepo structure for Strudel

--- node_modules/supradough/README.md
+++
@@ -1,3 +0,0 @@
-# supradough
-
-platform agnostic synth and sampler intended for live coding. a reimplementation of superdough.
--- node_modules/supradough/index.mjs
+++
@@ -1,5 +0,0 @@
-// import _workletUrl from './dough-worklet.mjs?url'; // only for dev (breaks for production build)
-import _workletUrl from './dough-worklet.mjs?audioworklet'; // only for prod (breaks in development?!)
-
-export * from './dough.mjs';
-export const workletUrl = _workletUrl;
